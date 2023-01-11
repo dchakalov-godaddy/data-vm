a75de502a6ab7b85ab4678667b67a73c2c82c1ac
dchakalov <dchakalov@godaddy.com>
2023-01-11 14:19:40 +0200

    test 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Headers.js';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Headers.js";
+import MainSection from "./components/MainSection.js";
 
 function App() {
-  return (
-    <Header />
-  )
+	return (
+		<>
+			<Header />
+			<MainSection />
+		</>
+	);
 }
 
 export default App;
---
+++ src/components/MainSection.js
@@ -0,0 +1,10 @@
+
+export default function MainSection() {
+
+
+	return (
+		<body className="main-section">
+            <h2>This is the main section</h2>
+		</body>
+	);
+}
