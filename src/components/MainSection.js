827d0afc64cee81616debb5850983f07dfb3dd89
dchakalov <dchakalov@godaddy.com>
2023-01-11 21:55:12 +0200

    test 1

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -1,6 +1,11 @@
 
-export default function MainSection() {
+export default async function MainSection() {
 
+	let file = await fetch("http://dc-data-vm.cloud.phx3.gdg/data/ams_private_subnets-json");
+
+	let jsonRes = await file.json()
+
+	console.log(jsonRes);
 
 	return (
 		<body className="main-section">
