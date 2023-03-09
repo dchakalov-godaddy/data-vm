f1d260092580e9acabcfde23f0da1ace3adb0d2b
dchakalov <dchakalov@godaddy.com>
2023-03-09 18:26:05 +0200

    updating network zone

--- src/components/Table.js
+++ src/components/Table.js
@@ -108,8 +108,10 @@ export default function Table({ data, type }) {
 											<tr className="table-row" key={index}>
 												{theadData.map((key, index) => {
 													console.log(row[key]);
-													return key === "network_name" ? (
-														<td key={index}>{row[key].split("_")[0].substring(2) || "None"}</td>
+													return key === "network_zone" ? (
+														<td key={index}>
+															{row[key].split("_")[0].substring(2) || "None"}
+														</td>
 													) : (
 														<td key={index}>{row[key] || "None"}</td>
 													);
