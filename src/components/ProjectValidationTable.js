027c26875cd7c8eae15ea820fd0c87af8a19dcdd
dchakalov <dchakalov@godaddy.com>
2023-03-23 17:57:33 +0200

    ref

--- src/components/ProjectValidationTable.js
+++ src/components/ProjectValidationTable.js
@@ -6,7 +6,7 @@ export default function ProjectValidationTable({ data, type }) {
 
 	let cloudData = data[cloudName];
 
-	let theadData = ["name", "id", "owning_group", "fips", "port_ips"];
+	let theadData = ["name", "id", "owning_group"];
 
 	return (
 		<div className="table-section">
